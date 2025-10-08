a3b4ff0df3e1f3234094b9e732ff244023575513
omar <omarqais225@gmail.com>
2025-10-08 12:13:54 +0300

    routes

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { Link } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Hello welcome to home page</Text>
+      <Link href="/login" style={{
+        marginTop: 10,
+      }} >
+        <TouchableOpacity
+          style={{
+            width: 200,
+            height: 50,
+            backgroundColor: "coral",
+            borderRadius: 15,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>Login Page</Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
+}
---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+const login = () => {
+    return (
+        <View>
+            <Text>Hello this is login page</Text>
+        </View>
+    )
+}
+export default login
